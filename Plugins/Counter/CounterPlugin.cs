c2e9443eff6acbb41994a9d3cb8a832aef32f4e5
Memich69 <michalmesika2@gmail.com>
2024-07-29 20:46:35 +0300

    first commit

---
+++ Plugins/Counter/CounterPlugin.cs
@@ -0,0 +1,27 @@
+﻿using BasePlugin.Interfaces;
+using BasePlugin.Records;
+using System;
+
+namespace Counter
+{
+    public class CounterPlugin : IPlugin
+    {
+        public static string _Id => "counter";
+        public string Id => _Id;
+
+        public PluginOutput Execute(PluginInput input)
+        {
+
+            if(input.PersistentData != null)
+            {
+                var lastCount = int.Parse(input.PersistentData);
+                var result = (lastCount + 1).ToString();
+                return new PluginOutput(result, result);
+            }
+            else
+            {
+                return new PluginOutput("Error: Input format is not valid", "Error");
+            }
+        }
+    }
+}
